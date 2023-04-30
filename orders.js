e4f59a6c0ad6a8f24b0dc6f9d5effaa7cada74ca
askdebb <53785246+askdebb@users.noreply.github.com>
2023-04-30 13:36:20 +0000

    admin dashboard buildt with html, css and js

---
+++ orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+{
+    productName: 'Ramatu Tanko',
+    productNumber: '4735343',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+{
+    productName: 'Drone',
+    productNumber: '47343',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+},
+{
+    productName: 'Laptop',
+    productNumber: '343',
+    paymentStatus: 'Due',
+    shipping: 'Declined'
+},
+{
+    productName: 'Scooter',
+    productNumber: '735343',
+    paymentStatus: 'Due',
+    shipping: 'Delivered'
+},
+{
+    productName: 'Horse',
+    productNumber: '25343',
+    paymentStatus: 'Paid',
+    shipping: 'Pending'
+},
+{
+    productName: 'House Fly',
+    productNumber: '95343',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+{
+    productName: 'Insecticide',
+    productNumber: '4735343',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+},
+{
+    productName: 'iphone 15 pro',
+    productNumber: '9898343',
+    paymentStatus: 'Paid',
+    shipping: 'Declined'
+}
+]
